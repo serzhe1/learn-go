293a8875819c8fe72c34c1c2146c90612094f796
s.ladygin <s.a.ladygin@yandex.ru>
2025-12-05 00:17:05 +0300

    implement Stringer and refactoring tests

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,9 +1,19 @@
 package pointers
 
+import "fmt"
+
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
